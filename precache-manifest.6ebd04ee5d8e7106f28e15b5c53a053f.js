4545ba6c881b5b69fe5a298d0b15e33b11aef0cf
msyahidin <muhammadsyahidien@gmail.com>
2020-10-02 12:40:04 +0000

    Deploying to gh-pages from  @ ca096cb1b262709b4298f66a496354620d21be1e 🚀

---
+++ precache-manifest.6ebd04ee5d8e7106f28e15b5c53a053f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f27c709ab005c84d2f487e62113faa4",
+    "url": "/next-ci/index.html"
+  },
+  {
+    "revision": "35eb8af4f071dc02044e",
+    "url": "/next-ci/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "069f48b038c5d99ab0ac",
+    "url": "/next-ci/static/js/2.4558504a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/next-ci/static/js/2.4558504a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35eb8af4f071dc02044e",
+    "url": "/next-ci/static/js/main.3c5567e5.chunk.js"
+  },
+  {
+    "revision": "7b99beaf43c31114a031",
+    "url": "/next-ci/static/js/runtime-main.049544d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/next-ci/static/media/logo.5d5d9eef.svg"
+  }
+]);
